a7a9445e7ab9e4635fc556bb59a05a92c89aa113
Martin Faartoft <martin.faartoft@gmail.com>
2015-05-08 14:01:04 +0200

    cors headers

--- api/settings.py
+++ api/settings.py
@@ -55,12 +55,14 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'api',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -68,6 +70,9 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
+
+#django-cors-headers
+CORS_ORIGIN_ALLOW_ALL = True 
 
 ROOT_URLCONF = 'api.urls'
 
