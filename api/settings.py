8ed3b3055989d69c1e0707a07b90896f07d1699a
Martin Faartoft <martin.faartoft@gmail.com>
2015-05-10 19:52:48 +0200

    exp -> 8 hours

--- api/settings.py
+++ api/settings.py
@@ -86,7 +86,7 @@ JWT_AUTH = {
     'JWT_VERIFY': True,
     'JWT_VERIFY_EXPIRATION': True,
     'JWT_LEEWAY': 0,
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=300),
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=8),
     'JWT_ALLOW_REFRESH': False,
     'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
 }
